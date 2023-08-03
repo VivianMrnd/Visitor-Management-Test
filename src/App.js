28c4f918a762ae754476ab13408da3ed49e46aae
euodtech <euodootech@gmail.com>
2023-08-03 16:49:40 +0800

    qr update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import QRCode from "react-qr-code";
+import { QrReader } from 'react-qr-reader';
 import { useState,useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const [data, setData] = useState('No result');
 
   const today = new Date().toLocaleString();
   const [logins, setLogins] = useState([]);
@@ -25,20 +27,82 @@ function App() {
     axios.get("http://localhost:8000/e-logbook/").then((res)=>{
       setLogins(res.data)
     })
-  },[])
+  })
 
   const form = {
     firstname:details.firstname,
-    lastname:details.lastname
+    lastname:details.lastname,
+    timein:details.timein,
+    timeout:details.timeout
   }
+  const loginperson = JSON.stringify(details);
+  const submitdetails = (e)=>{
+    e.preventDefault();
+    axios.post("http://localhost:8000/e-logbook/login", form).then(()=>{
+      setLogins([...logins, form])
+    })
+    
+  }
+
   return (
-    <div className="App container">
-      <form className="form m-5">
+    <div className="App container p-5">
+     
+              {logins.map((data, index)=>{
+            if(loginperson === data){
+                return(  
+                 
+                        <div key={index} className="row">    
+                    <div  className="col">
+                      {data.lastname}
+                    </div>
+                    <div className="col">
+                      {data.firstname}
+                    </div>
+                  </div>
+                 
+              
+                ) 
+              
+              //if
+            }else{
+              return(
+                <div>{loginperson}</div>
+              )
+            }
+
+              })}
+      
+
+     <form onSubmit={submitdetails} className="form m-5">
         <input placeholder="firstname" name="firstname" onChange={valueonchange}/>
         <input placeholder="lastname" name="lastname" onChange={valueonchange}/>
-      </form>
+        <button>submit</button>
+    </form>
+      <QRCode value={loginperson}/>
 
-      <QRCode value={JSON.stringify(form)}/>
+    {/*    <QrReader className="qrreader"
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          // if (!!error) {
+          //   console.info(error);
+          // }
+        }}
+      />
+      <p>{data}</p> */}
+      {
+        
+        logins.map((data)=>{
+            return(
+              <>
+              {JSON.stringify(data)}
+                </>
+            )
+        })
+      }
+     
     </div>
   );
 }
